4a984a1da30b98c70d73c68283d20242e0a72682
Przemysław Dziadowicz <pdzstwa@gmail.com>
2021-06-08 06:08:45 +0200

    no message

--- src/test/java/TestelkaFakestoreTests/Zadanie15/Zadanie15.java
+++ src/test/java/TestelkaFakestoreTests/Zadanie15/Zadanie15.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Zadanie15 {
 
